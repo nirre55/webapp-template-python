07fbfe5faf0b1b60072aea8fab25fcca2fd8f95b
Mehdi <mehdioulmi@gmail.com>
2025-04-25 19:24:26 -0400

    feat: add internationalization support with i18next and language switcher component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,31 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next';
+import { LanguageSwitcher } from './components';
 
 function App() {
-  const [message, setMessage] = useState('')
+  // const [message, setMessage] = useState('')
 
-  useEffect(() => {
-    fetch('http://localhost:8000/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/api/hello')
+  //     .then(res => res.json())
+  //     .then(data => setMessage(data.message))
+  // }, [])
+  //<h1 className="text-2xl font-bold text-blue-600">{message}</h1>
+  const { t } = useTranslation();
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold text-blue-600">{message}</h1>
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* Titre provenant des fichiers de traduction */}
+          <h1 className="text-2xl font-bold text-gray-800">{t('welcome')}</h1>
+          {/* Exemple de structure cle valeur {t('buttons.submit')} utilise le point pour acces au sous cles */}
+          {/* Composant pour changer de langue */}
+          <LanguageSwitcher />
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
 export default App
